c68585f7c7d5544aaf926d05954ec44e1dd72573
laramaktub <lara.cern@gmail.com>
2020-04-24 15:54:46 +0000

    Including the option of changing to NUMPY or JPG

--- examples_dicom/convert_to_jpg.py
+++ examples_dicom/convert_to_jpg.py
@@ -1,11 +1,14 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimage
-import pydicom as dicom
+import pydicom
+import ntpath
+import numpy as np
+import dicom_numpy
 import glob
 import os
 
-#Mark True if we want to obtain JPG format, in other case, it will be  in PNG 
-JPG = True
+#Mark True if we want to obtain NUMPY format, in other case, it will be in JPG
+NUMPY = False
 #Specify the dicom folder
 lstFilesDCM=glob.glob("*.dcm")
 #Specify the output jpg/png folder path
@@ -13,15 +16,24 @@ jpg_folder_path = "/home/ubuntu/NASH/dataset/examples_dicom/"
 #For each image in the folder path
 for i in lstFilesDCM:
     #Read each dicom file
-    ds = dicom.read_file(i)
-    #Read the dimension of the dicom file
-    pixel_array_numpy = ds.pixel_array
-    #In case we want JPG images, JPG is True
-    if JPG == True:
+    ds = pydicom.read_file(i)
+    if (NUMPY == True):    
+        #Maps filename with the base name of each dicom file in the folder 
+        filename=ntpath.basename(i)
+        #Load spaceing values:  dimension of dicom's  rows and columns
+        ConstPixelDims = (int(ds.Rows), int(ds.Columns))
+        #Array is sized based on -ConstPixelDims-
+        ArrayDicom = np.zeros(ConstPixelDims, dtype=ds.pixel_array.dtype)
+        #Store gross  image data
+        ArrayDicom[:,:] = ds.pixel_array
+        #Stacks in final_array array three times ArrayDicom, due to color images RBG
+        final_array=np.stack((ArrayDicom,)*3, axis=-1)
+        #Save numpy file with the file name and the array
+        np.save(filename, final_array)
+    else:
+        #Read the dimension of the dicom file
+        pixel_array_numpy = ds.pixel_array
         #Change format from dicom to JPG
         i = i.replace('.dcm', '.jpg')
-    #If we want a PNG format instead of JPG, so JPG is been set as False
-    else:
-        i = i.replace('.dcm', '.png')
-    #Save the new image in the output folder
-    plt.imsave(os.path.join(jpg_folder_path, i), ds.pixel_array, cmap=plt.cm.gray)
+       #Save the new image in the output folder
+        plt.imsave(os.path.join(jpg_folder_path, i), ds.pixel_array, cmap=plt.cm.gray)
